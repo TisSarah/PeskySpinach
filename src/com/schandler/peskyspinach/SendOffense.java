6608d01b197c0a496c7c6ecf983402bdb75f7e9c
Sarah Chandler <sarah@schandler.net>
2012-07-01 16:32:14 -0700

    Working menu items, added phone number type
    Hooked menu items up to their respective actions.
    Added phone number types for display along with phone number (ie.
    214-810-4540 (mobile))

--- src/com/schandler/peskyspinach/SendOffense.java
+++ src/com/schandler/peskyspinach/SendOffense.java
@@ -8,12 +8,16 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -49,26 +53,21 @@ public class SendOffense extends Activity {
 		Spinner spinner = (Spinner) findViewById(R.id.number_spinner); 
 		spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
 		
-		String[] projection = new String[] { Phone.NUMBER };
+		String[] projection = new String[] { Phone.NUMBER, Phone.TYPE, Phone.LABEL };
 		Cursor cursor = getContentResolver().query(Phone.CONTENT_URI, projection,  Phone.CONTACT_ID + " = " + mSpinachWorker.getContactId(), null, null);
 		int numPhoneNumbers = cursor.getCount();
 		String[] numbers = new String[numPhoneNumbers];
 		for(int i = 0; i < numPhoneNumbers; i++) {
 			cursor.moveToNext();
-			int columnIndex = cursor.getColumnIndex(Phone.NUMBER);
-			 numbers[i] = cursor.getString(columnIndex);
+			String phoneNum = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
+			int typeIndex = cursor.getInt(cursor.getColumnIndex(Phone.TYPE));
+			String label = cursor.getString(cursor.getColumnIndex(Phone.LABEL));
+			CharSequence phoneType = Phone.getTypeLabel(getResources(), typeIndex, label);
+			numbers[i] = phoneNum + " (" + phoneType + ")";
 		}
 		
 		ArrayAdapter<CharSequence> spinnerAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, numbers);
 		spinner.setAdapter(spinnerAdapter);
-		
-		Button smsButton = (Button) findViewById(R.id.send_sms_button); 
-		smsButton.setOnClickListener(new MyOnClickListener());
-		Button callButton = (Button) findViewById(R.id.make_call_button);
-		callButton.setOnClickListener(new MyOnClickListener());
-		
-		smsButton.setEnabled(false);
-		callButton.setEnabled(false);
 	}
 	
 	@Override
@@ -76,6 +75,22 @@ public class SendOffense extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.send_menu, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case R.id.sms:
+				mSpinachWorker.sendTheSMS(mSpinachWorker.getNumber(), mSpinachWorker.getMessage());
+				SendOffense.this.finish();
+				return true;
+			case R.id.call:
+				mSpinachWorker.makeTheCall(mSpinachWorker.getNumber(), mSpinachWorker.getMessage());
+				SendOffense.this.finish();
+				return true;
+			default: 
+				return super.onOptionsItemSelected(item);
+		}
 	}
 	
 	@Override
@@ -88,34 +103,13 @@ public class SendOffense extends Activity {
 	
 	public class MyOnItemSelectedListener implements OnItemSelectedListener {
 
-		public void onItemSelected(AdapterView<?> parent, View view, int pos,
-				long id) {
-			
+		public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {			
 			String selectedNumber = parent.getItemAtPosition(pos).toString();
 			mSpinachWorker.setNumber(selectedNumber);
-			
-			Button smsButton = (Button) findViewById(R.id.send_sms_button); 
-			Button callButton = (Button) findViewById(R.id.make_call_button);
-			
-			smsButton.setEnabled(true);
-			callButton.setEnabled(true);
 		}
 
 		public void onNothingSelected(AdapterView<?> arg0) {
 			// do nothing
-		}
-	}
-	
-	public class MyOnClickListener implements OnClickListener {
-
-		public void onClick(View view){
-			if(view.getId() == R.id.send_sms_button) {
-				mSpinachWorker.sendTheSMS(mSpinachWorker.getNumber(), mSpinachWorker.getMessage());
-			}
-			if(view.getId() == R.id.make_call_button) {
-				mSpinachWorker.makeTheCall(mSpinachWorker.getNumber(), mSpinachWorker.getMessage());
-			}
-			SendOffense.this.finish();
 		}
 	}
 }
